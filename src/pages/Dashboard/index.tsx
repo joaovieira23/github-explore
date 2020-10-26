6cc1d28d77bf6f67b119533e754245f37ec33fe2
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-10-26 17:36:11 -0300

    feat: stylize dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,63 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github.</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github.</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/45154356?s=460&u=3ae25f40bfcf6c661c7fad6f27ea1f471acf88ac&v=4"
+            alt="João Vieira"
+          />
+          <div>
+            <strong>redux-typescript</strong>
+            <p>Aplicação com configuração e entendimento do Redux e Redux</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/45154356?s=460&u=3ae25f40bfcf6c661c7fad6f27ea1f471acf88ac&v=4"
+            alt="João Vieira"
+          />
+          <div>
+            <strong>redux-typescript</strong>
+            <p>Aplicação com configuração e entendimento do Redux e Redux</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/45154356?s=460&u=3ae25f40bfcf6c661c7fad6f27ea1f471acf88ac&v=4"
+            alt="João Vieira"
+          />
+          <div>
+            <strong>redux-typescript</strong>
+            <p>Aplicação com configuração e entendimento do Redux e Redux</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
